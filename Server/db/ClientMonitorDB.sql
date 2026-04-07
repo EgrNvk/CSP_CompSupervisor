ab5f4f39b9ef7330799854fe33e35549bc909c00
EgrNvk <egrnvk@gmail.com>
2026-04-07 19:21:30 +0300

    Changed DB structure

--- Server/db/ClientMonitorDB.sql
+++ Server/db/ClientMonitorDB.sql
@@ -1,16 +1,18 @@
-CREATE DATABASE ClientMonitorDB;
+CREATE DATABASE [ClientMonitorDB];
 
-USE ClientMonitorDB;
+USE [ClientMonitorDB];
 
-CREATE TABLE clients (
-    id         INT IDENTITY(1,1) PRIMARY KEY,
-    ip         VARCHAR(45) NOT NULL UNIQUE,
-    hostname   NVARCHAR(255) NOT NULL DEFAULT '',
-    status     VARCHAR(20) NOT NULL DEFAULT 'Unknown',
-    first_seen DATETIME,
-    last_seen  DATETIME,
-    CONSTRAINT CK_clients_status
-        CHECK (status IN ('Online', 'Offline', 'Unknown'))
+CREATE TABLE [clients] (
+    [id] INT IDENTITY(1,1) PRIMARY KEY,
+    [ip] VARCHAR(45)   NOT NULL UNIQUE,
+    [hostname] NVARCHAR(255) NOT NULL DEFAULT '',
+    [seen] DATETIME,
+    [wait_sec] INT,
+    status AS (
+        CASE
+            WHEN DATEADD(SECOND, wait_sec, seen) > GETDATE()
+            THEN 'Online'
+            ELSE 'Offline'
+        END
+    )
 );
-
-SELECT * FROM clients
